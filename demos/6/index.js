4ebf62e20dc979e2da84f85921f3bd769ed268c5
Yvan Razafindramanana <mail@ludeo.net>
2020-02-19 11:54:27 +0100

    Mises à jour suite à présentation 13/02

---
+++ demos/6/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { liked: false };
+    }
+  
+    render() {
+      if (this.state.liked) {
+        return 'You liked this.';
+      }
+  
+      return e(
+        'button',
+        { onClick: () => this.setState({ liked: true }) },
+        'Like'
+      );
+    }
+  }
+
+const domContainer = document.querySelector('main#app');
+ReactDOM.render(React.createElement(LikeButton), domContainer);
